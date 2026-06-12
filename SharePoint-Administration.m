0b383bd8797e6adadcf11d2475b476160643a7c6
Sam Etemad <sam.etemad@yahoo.com>
2026-06-12 02:38:50 -0400

    Create SharePoint-Administration.m

---
+++ SharePoint-Administration.m
@@ -0,0 +1,70 @@
+SharePoint Administration
+
+Objective
+
+Manage SharePoint Online sites, permissions, document libraries and collaboration features within Microsoft 365.
+
+Environment
+
+* Microsoft SharePoint Online
+* Microsoft 365 Admin Center
+* Microsoft Entra ID
+
+Site Administration
+
+Configured and managed:
+
+* Team Sites
+* Communication Sites
+* Document Libraries
+* Site Permissions
+
+Permission Management
+
+Implemented access controls using:
+
+* Security Groups
+* Microsoft 365 Groups
+* SharePoint Permission Levels
+
+Permission levels included:
+
+* Owners
+* Members
+* Visitors
+
+Document Management
+
+Configured:
+
+* Folder structures
+* Version history
+* File sharing controls
+* Internal collaboration features
+
+Security Controls
+
+Applied:
+
+* Access reviews
+* Permission audits
+* External sharing restrictions
+* Secure document access policies
+
+Validation
+
+Verified:
+
+* User access permissions
+* File sharing functionality
+* Site accessibility
+* Collaboration workflows
+
+Skills Demonstrated
+
+* SharePoint Online
+* Microsoft 365 Administration
+* Permission Management
+* Document Management
+* Collaboration Platforms
+* Information Governance
